88e6824cd17c1ad37fd0a80430786e85240e92d4
Yunus ŞAHİN <74988950+ysahin1@users.noreply.github.com>
2023-08-07 15:07:41 -0400

    Update sam.sh

--- AutomatedWorkflows&BashScripts/sam.sh
+++ AutomatedWorkflows&BashScripts/sam.sh
@@ -7,3 +7,13 @@ for s in *.sam;
 done
 cd ./BAM
 bash featureCounts
+
+for f in $(ls ../StressInSpikes/SAM/*.sam);
+ do
+  var1=${f}
+  output=${var1##*/}
+  samtools view -Sb $f > ../StressInSpikes/Bam/${output}.bam
+  samtools view -h -b -f 4 ../StressInSpikes/Bam/${output}.bam > ../StressInSpikes/UnmappedBam/unmapped_${output}.bam
+  samtools view -h -b -q 20 ../StressInSpikes/Bam/${output}.bam > ../StressInSpikes/FilteredBam/${output}.bam
+done
+
